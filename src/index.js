d6df3dd0a91d1c97082497f80bf64cb9306efa95
cc0803 <simongeldbach@gmail.com>
2023-05-06 13:07:30 +0200

    Add background image and image loader

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import './styles.css';
+import tofu from './tofu.jpg';
 
 console.log("hello customer");
--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,10 @@ module.exports = {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
       },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader'],
+      },
     ],
   },
 };
