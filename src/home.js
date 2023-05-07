44dc5ae26e2baf236c378be2f841552492134331
cc0803 <simongeldbach@gmail.com>
2023-05-07 13:08:19 +0200

    Create dynamical home page

--- src/home.js
+++ src/home.js
@@ -1,1 +1,46 @@
+import './styles.css';
+
 const content = document.querySelector(".content");
+
+export default function createHome() {
+    console.log(content);
+    //content.classList.add("home");
+    content.appendChild(heading()); 
+
+    let container = createTextContainer();
+
+    // Create Container Heading
+    container.appendChild(createContainerHeading("A divine tofu expirience"));
+    // Create Container Paragraphs
+    container.appendChild(createParagraph("At the age of fifteen Tim was enlightened. It was his first time to eat tofu and scince then he made it his God given mission to bring every body the real spirit of tofu."))
+    container.appendChild(createParagraph("With creative new ways to serve tofu, tim made thousands of people fall in love with this divine food. He is most famous for his teriyaki tofu dish or his creative Tofu Ramen."))
+    container.appendChild(createParagraph('To carry on his mission, Tim opened "Tim`s Tofu Temple" as a way to bring evrybody the joy of Tofu.'))
+    container.appendChild(createParagraph("Tim`s Kitchen is opened daily from 12am till 10pm. Every Tofu lover and potential future tofu lover is welcome to visit us at this time."))
+
+    content.appendChild(container);
+}
+
+function heading() {
+    let heading = document.createElement("h1");
+    heading.textContent = "Tim`s Tofu Temple";
+    heading.classList.add("home-heading");
+    return heading;
+}
+
+function createParagraph(text) {
+    let p = document.createElement("p");
+    p.textContent = text;
+    return p;
+}
+
+function createTextContainer() {
+    let container = document.createElement("div");
+    container.classList.add("text-container");
+    return container;
+}
+
+function createContainerHeading(text) {
+    let cHeading = document.createElement("h2");
+    cHeading.textContent = text;
+    return cHeading;
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import './styles.css';
 import tofu from './tofu.jpg';
+import createHome from './home.js';
 
 console.log("hello customer");
+createHome();
