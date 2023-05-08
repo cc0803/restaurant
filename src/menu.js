361855895af23e356aae6230c16484ae85d0a726
cc0803 <simongeldbach@gmail.com>
2023-05-08 16:09:08 +0200

    Create logic for a menuContainer and dishContainer

--- src/menu.js
+++ src/menu.js
@@ -2,4 +2,22 @@ const content = document.querySelector(".content");
 
 export default function createMenu() {
     content.classList.add("menu");
+
+    let container = menuContainer();
+    container.appendChild(createDish()); 
+
+    content.appendChild(container);
+}
+
+function menuContainer() {
+    let container = document.createElement("div");
+    container.classList.add("menu-container");
+    return container;
+}
+
+function createDish() {
+    let dishContainer = document.createElement("div");
+
+    dishContainer.classList.add("dish-container");
+    return dishContainer;
 }
